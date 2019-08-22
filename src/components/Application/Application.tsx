dcb005135de34c34edd404a774b1ff4421c6a495
maintain <maintain@yandex-team.ru>
2019-08-22 15:07:38 +0300

    fix

--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -6,10 +6,13 @@ import GlobalStateProvider from '../GlobalStateProvider/GlobalStateProvider';
 export const FetchContext = React.createContext<{ [key: string]: {} }>({});
 
 export default function Application() {
+  const [fetchDataState] = React.useState({});
+  const [globalState] = React.useState({ first: 9, second: { b: 9 } });
+
   return (
     <React.Suspense fallback={<span>Loading...</span>}>
-      <FetchContext.Provider value={{}}>
-        <GlobalStateProvider value={{ first: 9, second: { b: 9 } }}>
+      <FetchContext.Provider value={fetchDataState}>
+        <GlobalStateProvider value={globalState}>
           <div>
             <First />
             <Second />
