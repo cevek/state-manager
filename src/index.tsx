7148abc38a2f71ab0581a75c31eef9fa10b5ca37
maintain <maintain@yandex-team.ru>
2019-08-22 17:24:15 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDom from 'react-dom';
 import Application from './components/Application/Application';
 import './style.css';
 
-window.__PRELOADED_FETCH_STATE__ = {};
-window.__PRELOADED_STATE__ = { first: 9, second: { b: 9 } };
+declare const PRELOADED_FETCH_STATE: object;
+declare const __PRELOADED_STATE__: object;
 
 ReactDom.render(<Application />, document.getElementById('root'));
