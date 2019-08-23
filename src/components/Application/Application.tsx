754fef53bcdf06a685c72c6862abe510faf49220
maintain <maintain@yandex-team.ru>
2019-08-23 04:51:09 +0300

    fix

--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -4,14 +4,11 @@ import Second from '../Second/Second';
 import GlobalStateProvider from '../GlobalStateProvider/GlobalStateProvider';
 import { FetchContext } from '../../utils/fetch';
 
-const __PRELOADED_FETCH_STATE__ = {};
-const __PRELOADED_STATE__ = { first: 9, second: { b: 9 } };
-
 export default function Application() {
   return (
     <React.Suspense fallback={<span>Loading...</span>}>
-      <FetchContext.Provider value={__PRELOADED_FETCH_STATE__}>
-        <GlobalStateProvider value={__PRELOADED_STATE__}>
+      <FetchContext.Provider value={window.__PRELOADED_FETCH_STATE__}>
+        <GlobalStateProvider value={window.__PRELOADED_STATE__}>
           <div>
             <First />
             <Second />
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,4 @@ import * as ReactDom from 'react-dom';
 import Application from './components/Application/Application';
 import './style.css';
 
-declare const PRELOADED_FETCH_STATE: object;
-declare const __PRELOADED_STATE__: object;
-
 ReactDom.render(<Application />, document.getElementById('root'));
